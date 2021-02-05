6252e15beba556972c8e2c69927e6a06f13de6c8
Siyabulela <siyabulelakhumalo079@gmail.com>
2021-02-05 11:08:15 +0200

    first commit

---
+++ lib/Screens/Welcome/components/body.dart
@@ -0,0 +1,38 @@
+import 'package:Connect_buddy/Screens/Welcome/about_screen.dart';
+import 'package:Connect_buddy/components/rounded_button.dart';
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Positioned(
+            child: Image.asset(
+              'assets/images/Connect+logo.png',
+              width: size.width * 0.9,
+            ),
+          ),
+          RoundedButton(
+            text: 'Get Started',
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return AboutScreen();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFF4B170);
+const kPrimaryLightColor = Color(0xFF052638);
